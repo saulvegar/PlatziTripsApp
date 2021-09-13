a73b14cf793cbf06c311fbff22f80bb0fea28bf9
Saul Vega <saul.vega@rappi.com>
2021-09-12 22:00:30 -0500

    First commit

---
+++ lib/description_place.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class DescriptionPlace extends StatelessWidget {
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+  
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
+
+  @override
+  Widget build(BuildContext context) {
+    final starHalf = Container(
+      margin: EdgeInsets.only(
+        top: 320.0,
+        right: 3.0,
+      ),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFF2C611)
+      ),
+    );
+
+    final starBorder = Container(
+      margin: EdgeInsets.only(
+        top: 320.0,
+        right: 3.0,
+      ),
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFF2C611)
+      ),
+    );
+
+    final star = Container(
+      margin: EdgeInsets.only(
+        top: 320.0,
+        right: 3.0,
+      ),
+      child: Icon(
+        Icons.star,
+        color: Color(0xFFF2C611)
+      ),
+    );
+
+    final description = Container(
+      margin: new EdgeInsets.only(
+        top: 20.0,
+        left: 20.0,
+        right: 20.0
+      ),
+      child: Text(
+        descriptionPlace,
+        style: TextStyle(
+          fontFamily: "Lato",
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF56575a)
+        ),
+      )
+    );
+
+    final titleStars = Row(
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: Text(
+            namePlace,
+            style: TextStyle(
+              fontFamily: "Lato",
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          children: [
+            star,
+            star,
+            star,
+            star,
+            star
+          ],
+        )
+      ],
+    );
+
+    return Column(
+      children: [
+        titleStars,
+        description
+      ],
+    );
+  }
+}
